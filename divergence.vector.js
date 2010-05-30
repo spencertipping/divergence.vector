a5d4333a934d55c3d8444a7065e2e79e5025fcf8
Spencer Tipping <spencer@spencertipping.com>
2010-05-30 17:42:03 -0600

    Numerous fixes with the help of the debugging module. Also removed tracing from vector methods.

--- divergence.vector.js
+++ divergence.vector.js
@@ -4,13 +4,14 @@
 d.rebase (function () {
   var range = n >$> (n > 0 ? range(n - 1) << n - 1 : []);
 
-  var     reduction = (left, right, join, op, n) >$> (left + (range(n) * (i >$> '@xs[#{i}] #{op} $0[#{i}]')).join(join) + right).fn(),
-              unify = n >$> (x >$> (x.constructor === Number ? range(n) * (_ >$> x) : x.xs ? x.xs : x)),
+  var         unify = n >$> (x >$> (x.constructor === Number ? range(n) * (_ >$> x) : x.xs || x)),
+          reduction = (left, right, join, op, n) >$> (left + (range(n) * (i >$> '@xs[#{i}] #{op} $0[#{i}]')).join(join) + right).compose (unify (n)),
       componentwise = reduction.fn ('new @constructor ([', '])', ','),
-           distance = _ >$> Math.sqrt (this % this),
-                dot = n >$> reduction ('', '', '+', '*', n).compose (unify (n)),
+                dot = reduction.fn ('', '', '+', '*'),
             initial = n >$> '[' + (range(n) * (_ >$> 0)).join (',') + ']',
-               four = n >$> '+-*/'.split('') * (op >$> op.maps_to (componentwise (op, n).compose (unify (n)))) / d.init;
+               four = n >$> '+-*/'.split('') * (op >$> op.maps_to (componentwise (op, n))) / d.init;
 
-  d.vector = range(6) * (n >$> d.init ('@xs = $0 || #{initial(n)}'.ctor (four (n), {'%': dot (n), distance: distance, toString: _ >$> '<#{this.xs.join(", ")}>'}),
+  d.vector = range(6) * (n >$> d.init ('@xs = $0 || #{initial(n)}'.ctor (four (n), {'&': v >$> this * ((this % v) / (v % v)), '|': v >$> this - (this & v), '%': dot (n),
+                                                                                    unit: _ >$> this / this.distance(),
+                                                                                    distance: _ >$> Math.sqrt (this % this), toString: _ >$> '<#{this.xs.join(", ")}>'}),
                                        {create: 'new d.vector[#{n}] (@_)'.fn()}))}) ();
