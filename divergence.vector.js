c984965247fb8deb146d4469b5d9f55353c2ee86
Spencer Tipping <spencer@spencertipping.com>
2010-05-30 21:19:10 -0600

    Using a quasi-array model for vectors. You can use Array.prototype.slice.call(v) if you want a proper array. (This after about three hours of reading about propertyIsEnumerable and such, which is a horrifying aspect of JavaScript)

--- divergence.vector.js
+++ divergence.vector.js
@@ -4,15 +4,17 @@
 d.rebase (function () {
   var range = n >$> (n > 0 ? range(n - 1) << n - 1 : []);
 
-  var         unify = n >$> (x >$> (x.constructor === Number ? range(n) * (_ >$> x) : x.xs || x)),
-          reduction = (left, right, join, op, n) >$> (left + (range(n) * (i >$> '@xs[#{i}] #{op} $0[#{i}]')).join(join) + right).compose (unify (n)),
-      componentwise = reduction.fn ('new @constructor ([', '])', ','),
+  var         unify = n >$> (x >$> (x.length ? x : this.constructor.create.apply (this, range(n) * (_ >$> x)))),
+          reduction = (left, right, join, op, n) >$> (left + (range(n) * (i >$> '$_[#{i}] #{op} $0[#{i}]')).join(join) + right).compose (unify (n)),
+      componentwise = reduction.fn ('new @constructor (', ')', ','),
                 dot = reduction.fn ('', '', '+', '*'),
-            initial = n >$> '[' + (range(n) * (_ >$> 0)).join (',') + ']',
+        constructor = n >$> (range(n) * (i >$> '$_[#{i}] = $#{i}') + ['@length = #{n}']).join(',').fn(),
                four = n >$> '+-*/'.split('') * (op >$> op.maps_to (componentwise (op, n))) / d.init;
 
-  d.vector = range(6) * (n >$> d.init ('@xs = $0 || #{initial(n)}'.ctor (four (n), {'&': v >$> this * ((this % v) / (v % v)), '|': v >$> this - (this & v), '%': dot (n),
-                                                                                  unit : _ >$> this / this.distance(),  distance : _ >$> Math.sqrt (this % this),
-                                                                              toString : _ >$> '<#{this.xs.join(", ")}>'}),
-                                       {create: 'new d.vector[#{n}] (@_)'.fn(),
-                                          from: 'new d.vector[#{n}] ([#{(range(n) * (i >$> arguments[0])).join(",")}])'.fn()}))}) ();
+  d.vector = range(6) * (n >$> d.init (constructor(n).ctor (four (n),
+                                                            {'&': v >$> this * ((this % v) / (v % v)), '|': v >$> this - (this & v), '%': dot (n),
+                                                           unit : _ >$> this / this.distance(),  distance : _ >$> Math.sqrt (this % this),
+                                                       toString : _ >$> '<#{Array.prototype.join.call(this, ", ")}>'}),
+                                       {create: 'new d.vector[#{n}] (#{(range(n) * (i >$> "$" + i)).join(",")})'.fn()}));
+  var v3 = d.vector[3];
+  d.init (v3.prototype, {'^': v >$> new v3 (this[1] * v[2] - this[2] * v[1], this[2] * v[0] - this[0] * v[2], this[0] * v[1] - this[1] * v[0])})}) ();
