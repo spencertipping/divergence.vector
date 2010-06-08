1cbbffd0e7fa851e1294acc609789fdf1eef6368
Spencer Tipping <spencer@spencertipping.com>
2010-06-08 01:08:51 -0600

    Converted d.vector to be a function from the dimension to a vector constructor for that dimension. The constructor is memoized, so subsequent accesses to d.vector(n) return referentially equivalent values.

--- divergence.vector.js
+++ divergence.vector.js
@@ -9,17 +9,18 @@ d.rebase (function () {
       componentwise = reduction.fn ('new @constructor (', ')', ','),
                 dot = reduction.fn ('', '', '+', '*'),
         constructor = n >$> (range(n) * (i >$> '$_[#{i}] = $#{i}') + ['@length = #{n}']).join(',').fn(),
-               four = n >$> '+-*/'.split('') * (op >$> op.maps_to (componentwise (op, n))) / d.init;
+               four = n >$> '+-*/'.split('') * (op >$> op.maps_to (componentwise (op, n))) / d.init,
+          generated = [];
 
-  d.vector = range(6) * (n >$> d.init (constructor(n).ctor (four (n),
+  d.vector = n >$> (generated[n] = generated[n] || d.init (constructor(n).ctor (four (n),
                                                             {'&': v >$> v * ((this % v) / (v % v)), '|': v >$> this - (this & v), '%': dot (n),
                                                         towards : (v, x) >$> this * (1.0 - x) + v * x,
                                                            unit : _ >$> this / this.distance(),  distance : _ >$> Math.sqrt (this % this),
                                                            into : _ >$> this.constructor.create.apply (this, d.arr(arguments) * (v >$> this % v.unit()).bind(this)),
                                                            from : _ >$> ((this, arguments) |$> ((t, a) >$> range(a.length) * (i >$> a[i] * t[i]))) / ((x, y) >$> x + y),
                                                        toString : _ >$> '<#{Array.prototype.join.call(this, ", ")}>'}),
-                                       {create: 'new d.vector[#{n}] (#{(range(n) * (i >$> "$" + i)).join(",")})'.fn()}));
-  var v3 = d.vector[3];
+                                                           {create: 'new (d.vector(#{n})) (#{(range(n) * (i >$> "$" + i)).join(",")})'.fn ()}));
+  var v3 = d.vector(3);
   d.init (v3.prototype, {'^': v >$> new v3 (this[1] * v[2] - this[2] * v[1], this[2] * v[0] - this[0] * v[2], this[0] * v[1] - this[1] * v[0]),
                       about : function (v, angle) {var b1 = v.unit(), o = this | b1, b2 = o.unit(), b3 = b1 ^ b2, l = o.distance();
                                                    return (this & b1) + b2 * (Math.cos(angle) * l) + b3 * (Math.sin(angle) * l)}})}) ();
